37f1703eecd732900f9d85f0391c615319e92b55
davidfb1305 <davidf23@ucm.es>
2025-09-26 08:22:50 +0200

    vector3d practica 0

--- skeleton/Vector3D.cpp
+++ skeleton/Vector3D.cpp
@@ -1,7 +1,11 @@
 #include "Vector3D.h"
+#include <cmath>
 
-Vector3D::Vector3D(float x, float y, float)
+Vector3D::Vector3D(float x, float y, float z)
 {
+	v3[0] = x;
+	v3[1] = y;
+	v3[2] = z;
 }
 
 Vector3D Vector3D::operator+(Vector3D aux)
@@ -16,10 +20,22 @@ Vector3D Vector3D::operator-(Vector3D aux)
 
 Vector3D Vector3D::operator*(Vector3D aux)
 {
-	return Vector3D();
+	return Vector3D(v3[0] * aux.v3[0] , v3[1] * aux.v3[1], v3[2] *aux.v3[2]);
+}
+
+void Vector3D::operator=(Vector3D aux)
+{
+	v3[0] = aux.v3[0];
+	v3[1] = aux.v3[1];
+	v3[2] = aux.v3[2];
 }
 
 Vector3D Vector3D::x(float aux)
 {
 	return Vector3D();
+}
+
+float Vector3D::module()
+{
+		return std::sqrt(v3[0] * v3[0] + v3[1] * v3[1] + v3[2] * v3[2]);
 }
--- skeleton/Vector3D.h
+++ skeleton/Vector3D.h
@@ -4,11 +4,13 @@ class Vector3D
 private:
 	float v3[3];
 public:
-	Vector3D(float x = 0, float y = 0, float = 0);
+	Vector3D(float x = 0, float y = 0, float z = 0);
 	Vector3D operator+(Vector3D aux);
 	Vector3D operator-(Vector3D aux);
 	Vector3D operator*(Vector3D aux);
+	void operator=(Vector3D aux);
 	Vector3D x(float aux);
+	float module();
 
 };
 
